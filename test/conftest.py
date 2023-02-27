b72c04f8fe171c72fb366f2f2c598370c8f75b35
JovieL25 <lllllinbb@gmail.com>
2023-02-26 22:33:03 -0500

    Added other samples

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,7 @@
 def pytest_addoption(parser):
     """Add argument parser to pytest, we can pass parameters to pytest.
     """
+    parser.addoption("--testCase", action="store", default=0)
     parser.addoption("--limit", action="store", default=0.05)
     parser.addoption("--qualifiedCoef", action="store", default=2.2)
     parser.addoption("--ffactor", action="store", default=3.5)
@@ -22,3 +23,7 @@ def pytest_generate_tests(metafunc):
     option_value = float(metafunc.config.option.ffactor)
     if 'ffactor' in metafunc.fixturenames and option_value is not None:
         metafunc.parametrize("ffactor", [option_value])
+
+    option_value = int(metafunc.config.option.testCase)
+    if 'testCase' in metafunc.fixturenames and option_value is not None:
+        metafunc.parametrize("testCase", [option_value])
--- test/test_image_preprocessing.py
+++ test/test_image_preprocessing.py
@@ -43,54 +43,102 @@ def transform_image(cropped_image):
     return median_img
 
 
-def get_cropped_volume_image():
+def get_cropped_volume_image(testCase):
     """Read the cropped volume
 
     Returns:
         SITK: The cropped volume sitk image
     """
-    abspath = os.path.abspath("test/sample/43681283_crop.npy")
-    nda = np.load(abspath)
-    return sitk.GetImageFromArray(nda)
+    sample = 43681283
+    if testCase == 1:
+        sample = 22429388
+    elif testCase == 2:
+        sample = 62023082
+    elif testCase == 3:
+        sample = 75962810
+    elif testCase == 4:
+        sample = "07323651"
+    elif testCase == 5:
+        sample = "05937785"
+    abspath = os.path.abspath("test/sample/{}_crop.vtk".format(sample))
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("VTKImageIO")
+    reader.SetFileName(abspath)
+    return reader.Execute()
 
 
-cropped_image = get_cropped_volume_image()
-cropped_image = transform_image(cropped_image)
-
-
-def read_desc_volume_image():
+def read_desc_volume_image(testCase):
     """Read the segmented descending aorta volume
 
     Returns:
         SITK: The segmented descending aorta sitk image
     """
-    abspath = os.path.abspath("test/sample/43681283_des.npy")
-    nda = np.load(abspath)
-    return sitk.GetImageFromArray(nda)
+    sample = 43681283
+    if testCase == 1:
+        sample = 22429388
+    elif testCase == 2:
+        sample = 62023082
+    elif testCase == 3:
+        sample = 75962810
+    elif testCase == 4:
+        sample = "07323651"
+    elif testCase == 5:
+        sample = "05937785"
+    abspath = os.path.abspath("test/sample/{}_des.vtk".format(sample))
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("VTKImageIO")
+    reader.SetFileName(abspath)
+    return reader.Execute()
 
 
-def read_asc_volume_image():
+def read_asc_volume_image(testCase):
     """Read the segmented ascending and descending aorta volume
 
 
     Returns:
         SITK: The segmented ascending and descending aorta sitk image
     """
-    abspath = os.path.abspath("test/sample/43681283_asc.npy")
-    nda = np.load(abspath)
-    return sitk.GetImageFromArray(nda)
+    sample = 43681283
+    if testCase == 1:
+        sample = 22429388
+    elif testCase == 2:
+        sample = 62023082
+    elif testCase == 3:
+        sample = 75962810
+    elif testCase == 4:
+        sample = "07323651"
+    elif testCase == 5:
+        sample = "05937785"
+    abspath = os.path.abspath("test/sample/{}_asc.vtk".format(sample))
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("VTKImageIO")
+    reader.SetFileName(abspath)
+    return reader.Execute()
 
 
-def read_final_volume_image():
+def read_final_volume_image(testCase):
     """Read the final segmented aorta volume
 
 
     Returns:
         SITK: The final segmented aorta sitk image
     """
-    abspath = os.path.abspath("test/sample/43681283_final.npy")
-    nda = np.load(abspath)
-    return sitk.GetImageFromArray(nda)
+    sample = 43681283
+    if testCase == 1:
+        sample = 22429388
+    elif testCase == 2:
+        sample = 62023082
+    elif testCase == 3:
+        sample = 75962810
+    elif testCase == 4:
+        sample = "07323651"
+    elif testCase == 5:
+        sample = "05937785"
+    abspath = os.path.abspath("test/sample/{}_final.vtk".format(sample))
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("VTKImageIO")
+    reader.SetFileName(abspath)
+    return reader.Execute()
 
 
 def DSC(ref_image, test_image):
@@ -123,12 +171,26 @@ def mean_square_error(ref_image, test_image):
     return npsum/np.count_nonzero(np.logical_or(ref_image, test_image))
 
 
-def test_compare_des(limit, qualifiedCoef, ffactor):
-    starting_slice = 820
-    aorta_centre = [18, 26]
-
-    # starting_slice = 700
-    # aorta_centre = [5, 60]
+def test_compare_des(limit, qualifiedCoef, ffactor, testCase):
+    starting_slice = 829
+    aorta_centre = [23, 28]
+    cropped_image = get_cropped_volume_image(testCase)
+    cropped_image = transform_image(cropped_image)
+    if testCase == 1:
+        starting_slice = 857
+        aorta_centre = [90, 34]
+    elif testCase == 2:
+        starting_slice = 1202
+        aorta_centre = [20, 15]
+    elif testCase == 3:
+        starting_slice = 962
+        aorta_centre = [38, 26]
+    elif testCase == 4:
+        starting_slice = 824
+        aorta_centre = [19, 20]
+    elif testCase == 5:
+        starting_slice = 919
+        aorta_centre = [29, 15]
     desc_axial_segmenter = AortaSegmenter(
         cropped_image=cropped_image,
         starting_slice=starting_slice, aorta_centre=aorta_centre,
@@ -141,22 +203,49 @@ def test_compare_des(limit, qualifiedCoef, ffactor):
 
     desc_axial_segmenter.begin_segmentation()
     test_image = desc_axial_segmenter.processing_image
-    ref_image = read_desc_volume_image()
+    ref_image = read_desc_volume_image(testCase)
     print("qualified slicefactor : {}".format(qualifiedCoef))
     print("filter factor : {}".format(ffactor))
     nda_ref = sitk.GetArrayFromImage(ref_image)
     nda_test = sitk.GetArrayFromImage(test_image)
     DSC_error = 1-DSC(nda_ref, nda_test)
     print_result(nda_ref, nda_test, SegType.descending_aorta)
+    writer = sitk.ImageFileWriter()
+    writer.SetImageIO("VTKImageIO")
+    writer.SetFileName("des_test_case3.vtk")
+    writer.Execute(test_image)
     assert (DSC_error < limit)
     return test_image
 
 
-def test_compare_asc(limit, qualifiedCoef, ffactor, processing_image=None):
+def test_compare_asc(
+    limit,
+    qualifiedCoef,
+    ffactor,
+    testCase,
+    processing_image=None
+        ):
+    cropped_image = get_cropped_volume_image(testCase)
+    cropped_image = transform_image(cropped_image)
     if not processing_image:
-        processing_image = read_desc_volume_image()
-    starting_slice = 733
-    aorta_centre = [87, 131]
+        processing_image = read_desc_volume_image(testCase)
+    starting_slice = 749
+    aorta_centre = [87, 130]
+    if testCase == 1:
+        starting_slice = 857
+        aorta_centre = [164, 168]
+    elif testCase == 2:
+        starting_slice = 1273
+        aorta_centre = [25, 88]
+    elif testCase == 3:
+        starting_slice = 1010
+        aorta_centre = [88, 161]
+    elif testCase == 4:
+        starting_slice = 873
+        aorta_centre = [76, 94]
+    elif testCase == 5:
+        starting_slice = 926
+        aorta_centre = [72, 116]
     asc_axial_segmenter = AortaSegmenter(
         cropped_image=cropped_image,
         starting_slice=starting_slice, aorta_centre=aorta_centre,
@@ -166,26 +255,48 @@ def test_compare_asc(limit, qualifiedCoef, ffactor, processing_image=None):
         processing_image=processing_image,
         seg_type=SegType.ascending_aorta
     )
-
     asc_axial_segmenter.begin_segmentation()
     test_image = asc_axial_segmenter.processing_image
-    ref_image = read_asc_volume_image()
+    ref_image = read_asc_volume_image(testCase)
     print("qualified slicefactor : {}".format(qualifiedCoef))
     print("filter factor : {}".format(ffactor))
     nda_ref = sitk.GetArrayFromImage(ref_image)
     nda_test = sitk.GetArrayFromImage(test_image)
     DSC_error = 1-DSC(nda_ref, nda_test)
     print_result(nda_ref, nda_test, SegType.ascending_aorta)
+    writer = sitk.ImageFileWriter()
+    writer.SetImageIO("VTKImageIO")
+    writer.SetFileName("asc_test_case2.vtk")
+    writer.Execute(test_image)
     assert (DSC_error < limit)
     return test_image
+
+
+def test_asc_and_final(
+    limit,
+    qualifiedCoef,
+    ffactor,
+    testCase,
+    processing_image=None
+        ):
+    ref_image = read_asc_volume_image(testCase)
+    test_image = read_final_volume_image(testCase)
+    nda_ref = sitk.GetArrayFromImage(ref_image)
+    nda_test = sitk.GetArrayFromImage(test_image)
+    DSC_error = 1-DSC(nda_ref, nda_test)
+    print_result(nda_ref, nda_test, SegType.ascending_aorta)
+    assert (DSC_error < limit)
 
 
 def test_compare_final_volume(
     limit,
     qualifiedCoef,
     ffactor,
+    testCase,
     processing_image=None
         ):
+    cropped_image = get_cropped_volume_image(testCase)
+    cropped_image = transform_image(cropped_image)
     if not processing_image:
         processing_image = read_asc_volume_image()
     if not qualifiedCoef:
@@ -203,7 +314,7 @@ def test_compare_final_volume(
     )
     sagittal_segmenter.begin_segmentation()
     test_image = sagittal_segmenter.processing_image
-    ref_image = read_final_volume_image()
+    ref_image = read_final_volume_image(testCase)
     print("qualified slicefactor : {}".format(qualifiedCoef))
     print("filter factor : {}".format(ffactor))
     nda_ref = sitk.GetArrayFromImage(ref_image)
@@ -214,8 +325,12 @@ def test_compare_final_volume(
     return test_image
 
 
-def test_prepared_segmenting_image(limit, qualifiedCoef, ffactor):
-    limit = float(limit)
-    processing_image = test_compare_des(limit)
-    processing_image = test_compare_asc(limit, processing_image)
-    processing_image = test_compare_final_volume(limit, processing_image)
+def test_prepared_segmenting_image(
+    limit,
+    qualifiedCoef,
+    ffactor,
+    testCase):
+    processing_image = test_compare_des(
+        limit, qualifiedCoef, ffactor, testCase)
+    processing_image = test_compare_asc(
+        limit, qualifiedCoef, ffactor, testCase, processing_image)
