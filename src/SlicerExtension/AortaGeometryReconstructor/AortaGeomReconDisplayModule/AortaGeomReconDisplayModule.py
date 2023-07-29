ea883a1c58e7dc42ea3c7124cad32503d53def0f
JovieL25 <lllllinbb@gmail.com>
2023-07-29 06:51:14 -0400

    Fixed SubjectHierarchyTreeView hiding issue. Fixed GetVTK path logic missing.

--- src/SlicerExtension/AortaGeometryReconstructor/AortaGeomReconDisplayModule/AortaGeomReconDisplayModule.py
+++ src/SlicerExtension/AortaGeometryReconstructor/AortaGeomReconDisplayModule/AortaGeomReconDisplayModule.py
@@ -154,6 +154,7 @@ class AortaGeomReconDisplayModuleWidget(ScriptedLoadableModuleWidget, VTKObserva
         self.logic = None
         self._parameterNode = None
         self._updatingGUIFromParameterNode = False
+        self._showWarning = True
 
     def setup(self):
         """
@@ -342,11 +343,12 @@ class AortaGeomReconDisplayModuleWidget(ScriptedLoadableModuleWidget, VTKObserva
         if self._parameterNode is None or self._updatingGUIFromParameterNode:
             return
 
-        self.showWarning()
-
         # Make sure GUI changes do not call updateParameterNodeFromGUI
         # (it could cause infinite loop)
         self._updatingGUIFromParameterNode = True
+        if self._showWarning:
+            self.showWarning()
+            self._showWarning = False
 
         self.ui.ascAortaSeed.coordinates = self._parameterNode.GetParameter(
             "ascAortaSeed")
@@ -505,6 +507,7 @@ class AortaGeomReconDisplayModuleWidget(ScriptedLoadableModuleWidget, VTKObserva
         self.ui.phaseLabel.text = AGR_phase.segment_desc_aorta.value
         self.ui.revertButton.enabled = True
         self.ui.warningConfirmButton.hide()
+        self.ui.SubjectHierarchyTreeView.show()
         self.ui.applyButton.show()
         self.ui.revertButton.show()
         self.ui.resetButton.show()
@@ -551,22 +554,31 @@ class AortaGeomReconDisplayModuleWidget(ScriptedLoadableModuleWidget, VTKObserva
         Depends on what state the module is in, the module output a vtk file under the root folder of Slicer application.
 
         """ # noqa
+
         sceneObj = slicer.mrmlScene
+        path = os.path.abspath(self.ui.outputPath.currentPath)
         if self._parameterNode.GetParameter("phase") == "1":
             size = len(slicer.util.getNodes("*cropped*", useLists=True))
             if size == 1:
                 volume = sceneObj.GetFirstNode("cropped", None, None, False)
                 self.logic.transform_image(volume)
                 self.logic.saveVtk("crop_volume.vtk", None, 1)
+                path = os.path.join(path, "crop_volume.vtk")
+                self.logic.saveVtk(path, None, 1)
+            else:
+                logging.warning("Cannot find cropped volume")
         elif self._parameterNode.GetParameter("phase") == "2":
             size = len(slicer.util.getNodes("*Seg*", useLists=True))
             if size == 1:
                 volume = sceneObj.GetFirstNode("Seg", None, None, False)
+                path = os.path.join(path, volume.GetName())
                 self.logic.saveVtk(
-                    "{}.vtk".format(volume.GetName()),
+                    "{}.vtk".format(path),
                     volume,
                     2
                 )
+            else:
+                logging.warning("Cannot find Segmentation label volume")
 
     def onRevertButton(self):
         """
@@ -608,7 +620,7 @@ class AortaGeomReconDisplayModuleWidget(ScriptedLoadableModuleWidget, VTKObserva
         stop_limit = self._parameterNode.GetParameter(
             "stop_limit")
         threshold_coef = self._parameterNode.GetParameter(
-            "threshold_coef")
+            "threshold_coef").format(price = 49)
         rms_error = self._parameterNode.GetParameter("rms_error")
         no_ite = self._parameterNode.GetParameter("no_ite")
         curv_scaling = self._parameterNode.GetParameter(
@@ -639,11 +651,11 @@ class AortaGeomReconDisplayModuleWidget(ScriptedLoadableModuleWidget, VTKObserva
                 )
                 sitkUtils.PushVolumeToSlicer(
                     image,
-                    name="Seg_th{}_k{}_c{}_p{}".format(
-                        threshold_coef,
-                        kernel_size,
-                        curv_scaling,
-                        prop_scaling),
+                    name="Seg_th{tc:.2f}_k{ks}_c{c}_p{p}".format(
+                        tc=float(threshold_coef),
+                        ks=kernel_size,
+                        c=curv_scaling,
+                        p=prop_scaling),
                     className="vtkMRMLScalarVolumeNode"
                 )
 
